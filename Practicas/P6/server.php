f631728a55df1a5904115226bfb91590e11ab24d
Jose Alberto Zurera Reyes <jzr0001@alu.medac.es>
2021-12-20 20:34:18 +0100

    Creación de Práctica 6

---
+++ Practicas/P6/server.php
@@ -0,0 +1,9 @@
+<?php
+$nombre = $_POST['name'];
+$apellido1 = $_POST['apellido1'];
+$apellido2 = $_POST['apellido2'];
+$genero = $_POST['genero'];
+$fechaNacimiento = $_POST['fechaNacimiento'];
+$email = $_POST['email'];
+$password = $_POST['password'];
+?>
